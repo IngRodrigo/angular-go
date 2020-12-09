f4835599385ab207e2c57a744b70599ea2f71235
Rodrigo Jesus Sanchez <rodrigojesus-sanchez@aj.com.py>
2020-12-09 12:51:30 -0300

    Initial commit

---
+++ myApp/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  //todo lo que esta aqui se podra usar en el template html
+  title = 'Lista de estudiantes';
+  listEstudiantes:any[]=[
+  {nombre:"Rodrigo", estado:"aprobado"},
+  {nombre:"Juan", estado:"Regular"},
+  {nombre:"Jorge", estado:"aprobado"}
+  ];
+
+  mostrar=false;
+  
+  mostrarLista(){
+    this.mostrar=!this.mostrarLista;
+  }
+
+}
